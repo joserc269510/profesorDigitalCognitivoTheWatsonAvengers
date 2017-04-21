34b8dcf8fc8ea08739dc7adb990e419aef648c5b
joserc269510 <jose26jose26>
2017-04-21 14:48:22 -0600

    14:48 - 21 abril

--- src/main/java/Integracion/BaseDeDatos.java
+++ src/main/java/Integracion/BaseDeDatos.java
@@ -7,38 +7,30 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class BaseDeDatos {
+	
+	public static final String url ="jdbc:postgresql://echo.db.elephantsql.com:5432/pbfpjmdk";
+	public static final String username = "pbfpjmdk";
+	public static final String password = "e9C7qfGQpYjxj3UtrGHPfmehbBuO73I3";
 
-  public static void main(String[] args) {
-	        try {
+	
+	public void select(String query){//selects
+	       	try {
 	            Class.forName("org.postgresql.Driver");
 	        }
 	        catch (java.lang.ClassNotFoundException e) {
 	            System.out.println(e.getMessage() + "hola");
 	        }
-//postgres://username:password@hostname/databasename
-//postgres://roxvyxky:CaCHvDgMsN8NKdbu6dvD@babar.elephantsql.com:5432/roxvyxky
-	       
-//"jdbc:postgresql://host:port/database"
-//"jdbc:postgresql://echo.db.elephantsql.com:5432/pbfpjmdk
-	        
-//String url = "jdbc:postgresql://" + uri.getHost() + ":" + uri.getPort() + uri.getPath();
-	        
-	        String url ="jdbc:postgresql://echo.db.elephantsql.com:5432/pbfpjmdk";
-	        String username = "pbfpjmdk";
-	        String password = "e9C7qfGQpYjxj3UtrGHPfmehbBuO73I3";
-
 	        try {
 	        	System.out.println("daskdjkasjdsakl");
 	            Connection db = DriverManager.getConnection(url, username, password);
 	            Statement st = db.createStatement();
-	            ResultSet rs = st.executeQuery("SELECT * FROM public.profesor LIMIT 100");
+	            ResultSet rs = st.executeQuery(query);
 	            while (rs.next()) {
 	                System.out.print("Column 1 returned ");
 	                System.out.println(rs.getString(2));
 	                System.out.print("Column 2 returned ");
 	                System.out.println(rs.getString(3));	              
 	            }
-	           
 	            rs.close();
 	            st.close();
 	            }
@@ -46,6 +38,32 @@ public class BaseDeDatos {
 	            System.out.println(e.getMessage() + "adios");
 	        }
 	    }
-
+	
+	public void insertDelete(String query){
+       	try {
+            Class.forName("org.postgresql.Driver");
+        }
+        catch (java.lang.ClassNotFoundException e) {
+            System.out.println(e.getMessage() + "hola");
+        }
+       	
+       	try{
+       		System.out.println("daskdjkasjdsakl");
+            Connection db = DriverManager.getConnection(url, username, password);
+            Statement st = db.createStatement();
+            st.executeUpdate(query);  
+            st.close();
+       	}
+       	catch(java.sql.SQLException e){
+       		System.out.println(e.getMessage() + "adios");
+       	}
 	}
-
+	
+	public void update(String query){
+		
+	}
+  
+  	public static void main(String[] args) {
+	
+  	}
+}
